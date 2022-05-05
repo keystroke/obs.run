5aa9b281609132a5741acf1a3a2dcf642190dcb4
Bryant <bryant@keystroke.cloud>
2022-05-05 04:03:56 -0700

    Update run.js

--- run.js
+++ run.js
@@ -1,1 +1,12 @@
-console.log("Hello world from run!");
+console.log(Deno.args);
+
+window.setImmediate = func => setTimeout(func, 0);
+
+const { Runtime } = await import(
+  "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js"
+);
+
+const { default: define } = await import(getNotebookUrl(`&d=${Date.now()}`));
+
+const main = await new Runtime().module(define).value(CELL);
+return await main(Deno);
