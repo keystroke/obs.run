b6960dedd032d39950218f3113e13e5110ddc2d8
Bryant <bryant@keystroke.cloud>
2023-02-02 21:21:35 -0800

    error handling; default watch -> 31000ms

--- watch.js
+++ watch.js
@@ -26,7 +26,7 @@ function getArgs() {
     a => a.startsWith("--") && a !== reload && a !== watch
   );
   const notebook = Deno.args.filter(a => !a.startsWith("--"))[0];
-  if (watch) watch = Number(watch.split("=", 2)[1]) || 7000;
+  if (watch) watch = Number(watch.split("=", 2)[1]) || 31000;
   if (!reload) reload = "--reload=https://api.observablehq.com";
   else if (reload !== "--reload") reload += ",https://api.observablehq.com";
   return { watch, reload, permissions, notebook };
@@ -34,8 +34,13 @@ function getArgs() {
 
 async function getEtag({ notebook }) {
   const url = `https://api.observablehq.com/${notebook}.js?v=3`;
-  const response = await fetch(url);
-  return response.headers.get("etag");
+  try {
+    const response = await fetch(url);
+    return response.headers.get("etag");
+  } catch (error) {
+    console.warn(error);
+    return null;
+  }
 }
 
 function startWorker({ reload, permissions, notebook }) {
