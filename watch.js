dccf8ce5ab91584f01dd32b51b32e0514bfe11c0
Bryant <bryant@keystroke.cloud>
2023-02-04 04:24:16 -0800

    support env vars and command line args

--- watch.js
+++ watch.js
@@ -20,15 +20,23 @@ if (watch) {
 import(`https://obs.run/${notebook}`);
 
 function getArgs() {
-  let watch = Deno.args.filter(a => a.startsWith("--watch"))[0];
-  let reload = Deno.args.filter(a => a.startsWith("--reload"))[0];
-  const permissions = Deno.args.filter(
-    a => a.startsWith("--") && a !== reload && a !== watch
-  );
-  const notebook = Deno.args.filter(a => !a.startsWith("--"))[0];
-  if (watch) watch = Number(watch.split("=", 2)[1]) || 31000;
-  if (!reload) reload = "--reload=https://api.observablehq.com";
-  else if (reload !== "--reload") reload += ",https://api.observablehq.com";
+  let watch = Deno.args.filter(a => a.startsWith("--watch"))[0]
+    || Deno.env.get("WATCH").filter(i => i).map(i => `--watch=${i}`)[0];
+  let reload = Deno.args.filter(a => a.startsWith("--reload"))[0]
+    || [Deno.env.get("RELOAD")].filter(i => i).map(i => `--reload=${i}`)[0]
+    || "--reload=https://api.observablehq.com";
+  let permissions = Deno.args
+    .filter(a => a.startsWith("--"))
+    .filter(a => a !== watch && a !== reload)
+    .join(" ")
+      || Deno.env.get("PERMISSIONS")
+      || "";
+  const notebook = Deno.args.filter(a => !a.startsWith("--"))[0]
+    || Deno.env.get("NOTEBOOK");
+  if (watch)
+    watch = Number(watch.split("=", 2)[1]) || 31000;
+  if (reload != "--reload" && !reload.includes("https://api.observablehq.com"))
+    reload += ",https://api.observablehq.com";
   return { watch, reload, permissions, notebook };
 }
 
@@ -36,9 +44,10 @@ async function getEtag({ notebook }) {
   const url = `https://api.observablehq.com/${notebook}.js?v=3`;
   try {
     const response = await fetch(url);
+    if (!response.ok) console.warn(`${response.status} ${url}`, await response.text());
     return response.headers.get("etag");
   } catch (error) {
-    console.warn(error);
+    console.warn(url, error);
     return null;
   }
 }
@@ -48,7 +57,7 @@ function startWorker({ reload, permissions, notebook }) {
     "deno",
     "run",
     reload,
-    ...permissions,
+    permissions,
     `https://obs.run/${notebook}`,
   ];
   console.log(`$ ${cmd.join(" ")}`);
